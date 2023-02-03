557ab388fb79a1eb4852940824d908692759315a
JeevarehaR <jeev.reh@gmail.com>
2023-02-03 12:11:30 +0530

    Qualifications dialog box added

--- src/App.js
+++ src/App.js
@@ -13,6 +13,12 @@ import { AboutMe } from "./AboutMe";
 import { Skills } from "./Skills";
 import { ProjectsCard } from "./ProjectsCard";
 import { ContactMe } from "./ContactMe";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+import Slide from "@mui/material/Slide";
 
 export function App() {
   const [mode, setMode] = useState("dark");
@@ -22,12 +28,56 @@ export function App() {
       mode: mode,
     },
   });
+  const [open, setOpen] = useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
 
   return (
     <ThemeProvider theme={theme}>
       <Paper style={{ borderRadius: 0, minHeight: "100vh" }} elevation={0}>
         <div className="App">
           <Toolbar sx={{ float: "right" }}>
+            <Button
+              variant="inherit"
+              sx={{
+                textTransform: "none",
+                fontFamily: "Rouge Script, cursive",
+                fontSize: "24px",
+              }}
+              onClick={handleClickOpen}
+            >
+              Qualifications
+            </Button>
+            <Dialog
+              open={open}
+              onClose={handleClose}
+              aria-describedby="alert-dialog-slide-description"
+            >
+              <DialogTitle sx={{ color: " #555A8f", fontWeight: "bold" }}>
+                B.Tech/Information Technology
+              </DialogTitle>
+              <DialogContent>
+                <DialogContentText id="alert-dialog-slide-description">
+                  I've done my Bachelors in Information Technology (2016 - 2020)
+                  in VCET, Madurai. While, HSC and SSLC in NAMMHSS, Ramnad.
+                  After graduation, I self-trained myself and got an Udemy
+                  Certificate as well I completed Full (MERN) Stack Development
+                  Course from GUVI in 2022.
+                </DialogContentText>
+              </DialogContent>
+              <DialogActions>
+                <Button color="secondary" onClick={handleClose}>
+                  Pop
+                </Button>
+              </DialogActions>
+            </Dialog>
+
             <Button
               sx={{
                 marginLeft: 2,
--- src/ProjectsCard.js
+++ src/ProjectsCard.js
@@ -6,6 +6,7 @@ import CardMedia from "@mui/material/CardMedia";
 import { CardActionArea, CardActions } from "@mui/material";
 import ChevronRightIcon from "@mui/icons-material/ChevronRight";
 import moviesImg from "./movies app.png";
+import crmImg from "./crmImg.png";
 import { Link } from "@mui/material";
 
 const projects = [
@@ -16,44 +17,61 @@ const projects = [
       "Watch, Add, Delete and Edit movies of your own collection with this app.",
     link: "https://jeevr-movies.netlify.app/movies",
   },
+  {
+    name: "CRM App",
+    image: crmImg,
+    content:
+      "Sign up,Login,Interact with the tutorial sessions and experience knowledge!",
+    link: "",
+  },
 ];
 
 export function ProjectsCard() {
   return (
-    <div className="projects-card" style={{ marginBottom: "40px" }}>
+    <div>
       <h2>Projects</h2>
-      <br />
+      <div
+        className="projects-card"
+        style={{
+          margin: "40px 0",
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "space-evenly",
+        }}
+      >
+        <br />
 
-      {projects.map((proj) => (
-        <Card sx={{ maxWidth: 345 }}>
-          <CardActionArea>
-            <CardMedia
-              component="img"
-              height="186"
-              image={proj.image}
-              alt="movies app"
-            />
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="div">
-                {proj.name}
-              </Typography>
-              <Typography variant="body2" color="text.secondary">
-                {proj.content}
-              </Typography>
-            </CardContent>
-          </CardActionArea>
-          <CardActions>
-            <Link
-              className="link"
-              href={proj.link}
-              sx={{ color: "#7b7e81", textDecoration: "none" }}
-            >
-              Read more
-            </Link>
-            <ChevronRightIcon fontSize="inherit" />
-          </CardActions>
-        </Card>
-      ))}
+        {projects.map((proj) => (
+          <Card sx={{ maxWidth: 345 }}>
+            <CardActionArea>
+              <CardMedia
+                component="img"
+                height="186"
+                image={proj.image}
+                alt="movies app"
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  {proj.name}
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  {proj.content}
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+            <CardActions>
+              <Link
+                className="link"
+                href={proj.link}
+                sx={{ color: "#7b7e81", textDecoration: "none" }}
+              >
+                Read more
+              </Link>
+              <ChevronRightIcon fontSize="inherit" />
+            </CardActions>
+          </Card>
+        ))}
+      </div>
     </div>
   );
 }
